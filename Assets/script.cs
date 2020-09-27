ad220aa601d1e4e23457b0f43d21f4fdcdedba44
felocSsS <vlad.shipilov.felocs@gmail.com>
2020-09-27 23:33:02 +0300

    Second Commit

---
+++ Assets/script.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class script : MonoBehaviour
+{
+	public Button Button1;
+
+	void Start()
+	{
+		Button btn = Button1.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Debug.Log("You have clicked the button!");
+	}
+}
