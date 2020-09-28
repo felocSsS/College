4235ac27cbdba8c5563fec2c088adc347002a4c6
felocSsS <vlad.shipilov.felocs@gmail.com>
2020-09-28 19:29:51 +0300

    рабочий коммит

--- Assets/script.cs
+++ Assets/script.cs
@@ -4,16 +4,8 @@ using System.Collections;
 
 public class script : MonoBehaviour
 {
-	public Button Button1;
-
-	void Start()
-	{
-		Button btn = Button1.GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick);
-	}
-
-	void TaskOnClick()
-	{
-		Debug.Log("You have clicked the button!");
-	}
+	public void Button_Click()
+    {
+        Debug.Log("hello");
+    }
 }
